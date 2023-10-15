2364aeab09dc6050a774d2f7219f42a60b10e096
Maryam-Ketatni-004 <maryamketatni00.com>
2023-10-15 05:57:21 -0700

    printf-str

--- main.h
+++ main.h
@@ -7,6 +7,21 @@
 #include <limits.h>
 #include <unistd.h>
 
+
+/**
+ * struct convrt - defne a struct for symbols and fnction
+ *
+ * @dr: The operator
+ * @g: The function associated
+ */
+
+struct convrt
+{
+	char *dr;
+	int (*g)(va_list);
+};
+typedef struct convrt convr_t;
+
 /* print_string */
 
 int printf_strg(va_list val);
