f5d0c7eb671b17a67532c008b85ede6961bc84c0
Maryam-Ketatni-004 <maryamketatni00.com>
2023-10-15 05:38:51 -0700

    printf-str

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+/* print_string */
+
+int printf_strg(va_list val);
+
+
+
+#endif
