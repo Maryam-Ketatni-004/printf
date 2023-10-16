3cf25ed05ff4c3153299e3fe96097fd8ea1c595b
Maryam-Ketatni-004 <maryamketatni00.com>
2023-10-16 09:12:56 -0700

    printf task0

---
+++ _print.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * _print - The printf fuction.
+ * @format: A constant char.
+ *
+ * Return: The number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	int charcount = 0;
+	va_list ptr;
+
+	va_start(ptr, format);
+	if (format == NULL)
+		return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	charcount = _printf_helper(format, ptr, charcount);
+	va_end(ptr);
+	return (charcount);
+}
+/**
+ * _printf_helper - helper function.
+ * @format: A constant char.
+ * @ptr: pointer.
+ * @charcount: number of chars.
+ * Return: The number of characters printed.
+ */
+int _printf_helper(const char *format, va_list ptr, int charcount)
+{
+	while (*format)
+	{
+		if ((*format == '%') && (*(format + 1) == 'c'))
+		{
+			charcount += print_char(ptr);
+			format += 2;
+		}
+		else if ((*format == '%') && (*(format + 1) == 's'))
+		{
+			charcount += print_string(ptr);
+			format += 2;
+		}
+		else if ((*format == '%') && (*(format + 1) == '%'))
+		{
+			charcount += print_pourcentage();
+			format += 2;
+		}
+		else if ((*format == '%') && (*(format + 1) == 'i'))
+		{
+			charcount += print_int(ptr);
+			format += 2;
+		}
+		else
+		{
+			write(1, format, 1);
+			charcount++;
+			format++;
+		}
+	}
+	return (charcount);
+}
--- main.h
+++ main.h
@@ -1,103 +1,16 @@
-#ifndef _PRINTF_H
-#define _PRINTF_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
 
-#define OUTPUT_BUF_SIZE 1024
-#define BUF_FLUSH -1
-
-#define NULL_STRING "(null)"
-#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-#define CONVERT_LOWERCASE      1
-#define CONVERT_UNSIGNED       2
-
-/**
- * struct parameters - parameters struct
- *
- * @unsign: flag if unsigned value
- *
- * @plus_flag: on if plus_flag specified
- * @space_flag: on if hashtag_flag specified
- * @hashtag_flag: on if _flag specified
- * @zero_flag: on if _flag specified
- * @minus_flag: on if _flag specified
- *
- * @width: field width specified
- * @precision: field precision specified
- *
- * @h_modifier: on if h_modifier is specified
- * @l_modified: on if l_modifier is specified
- *
- */
-typedef struct parametrs
-{
-	unsigned int unsign      : 1;
-	unsigned int plus_flag   : 1;
-	unsigned int space_flag  : 1;
-	unsigned int hashtag_flag : 1;
-	unsigned int minus_flag  : 1;
-	unsigned int zero_flag   : 1;
-
-	unsigned int width;
-	unsigned int precision;
-	unsigned int h_modifier    : 1;
-	unsigned int l_modifier    : 1;
-} params_t;
-
-/**
- * struct specifier - Srtuct token
- *
- *
- * @specifier: format token
- * @f: the function associated
- */
-
-typedef struct specifier
-{
-	char *specifier;
-	int (*f)(va_list, params_t *);
-} spesifier_t;
-
-int _puts(char *str);
-int _putchar(int c);
-
-int print_char(va_list ap, params_t *params);
-int print_int(va_list ap, params_t *params);
-int print_string(va_list ap, params_t *params);
-int print_percent(va_list ap, params_t*params);
-int print_S(va_list ap, params_t *params);
-
-char *convert(long int num, int base, int flags, params_t *params);
-int print_unsigned(va_list ap, params_t *params);
-int print_address(va_list ap, params_t *params);
-
-int (*get_specifier(char *s))(va_list ap, params_t *params);
-int get_print_func(char *s, va_list ap, params_t *params);
-int get_flag(char *s, params_t *params);
-int get_modifier(char *s, params_t *params);
-int *get_width(char *s, params_t *params, va_list ap);
-
-
-
-
-
-
-
-
-
-
-int print_from_to(char *start, char *stop, char *except);
-
-
-
-
-void init_params(params_t *params, va_list ap);
-char *get_precision(char *pp, params_t *params, va_list ap);
+int print_char(va_list ptr);
+int print_int(va_list ptr);
+int print_string(va_list ptr);
+int print_pourcentage(void);
+int _print_helper(const char *format, va_list ptr, int charcount);
 int _printf(const char *format, ...);
-
 
 #endif
